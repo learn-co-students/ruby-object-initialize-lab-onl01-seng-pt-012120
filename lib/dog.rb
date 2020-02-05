27f22e551ddec6a4776560a8d087c35bf34a2b15
antromo <anthonyroma741@gmail.com>
2020-02-05 12:38:03 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+
+    def initialize(name , breed = "Mutt")
+      @breed = breed
+      @name = name
+    end 
+   
+   
+   
+   
+
+    def name=(name , breed)
+    @name = name
+    @breed = breed
+    end
+
+    def breed
+      @breed 
+    end
+
+    def name
+    @name
+    end
+end
