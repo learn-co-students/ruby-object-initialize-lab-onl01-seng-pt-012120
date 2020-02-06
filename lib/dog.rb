ebd58798b3333bef17944931703d31a5e07e5c81
fadimata tillman <fadi.tillman@gmail.com>
2020-02-06 17:19:31 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+
+class Dog
+def initialize (name, breed = "Mutt")
+@name = name
+@breed = breed
+end
+def name_breed (name, breed = "Mutt")
+@name = name
+@breed = breed
+end
+def name_breed (name, breed = "Mutt")
+@name
+@breed
+end
+end
