979460314beb6a263c4956429939b5ce23403a04
Patricia C <patricia@patriciaco.com>
2020-02-15 07:44:10 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+
+class Dog
+  
+   def initialize(name, breed = "Mutt")
+      @name = name
+      @breed = breed
+end
+
+    def name=(name)
+      @name
+      @breed
+  end
+  
+  def name
+    "#{@name} #{@breed}"
+   end
+ end
+
+fido = Dog.name
+
+
+
+
+
+
+
